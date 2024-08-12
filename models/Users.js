4b7e9319b0ed2ff091890cab993a038defcfb12a
unknown <aseeraachu895@gmail.com>
2024-08-12 08:27:39 +0530

    project start

---
+++ models/Users.js
@@ -0,0 +1,15 @@
+const Mongoose=require("mongoose")
+
+const UserSchema =Mongoose.Schema(
+    {
+        NamedNodeMapphone
+        Village
+        Place
+        Pincode
+        HouseNo
+        MissingDate
+        AadharNo
+        Gender
+        Age
+    }
+)
