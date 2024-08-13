f78d58d24af6eea7406c2ad8f6d635d4e7541aea
unknown <aseeraachu895@gmail.com>
2024-08-14 02:44:46 +0530

    project start

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
-const Express=require("express")
-const Mongoose=require("mongoose")
-const Bcrypt=require("bcrypt")
-const Cors=require("cors")
-const jsonwebtoken=require("jsonwebtoken")
-let app=Express()
-app.get("/",(req,res)=>{
-    res.send("hello")
+const express=require("express")
+const mongoose=require("mongoose")
+const bcrypt=require("bcrypt")
+const cors=require("cors")
+const loginModel = require("./models/admin")
+const app=express()
+app.use(cors())
+app.use(express.json())
+mongoose.connect("mongodb+srv://aseera:aseera@cluster0.x0tifel.mongodb.net/rescueDB?retryWrites=true&w=majority&appName=Cluster0")
+
+
+app.post("/userSignUp",(req,res)=>{
+    let input=req.body
+    let hashedpassword=bcrypt.hashSync(input.password,10)
+   //console.log(hashedpassword)
+   input.password=hashedpassword
+   console.log(input)
+   let result=new loginModel(input)
+   result.save
+   res.json({"status":"success"})
 })
 
-app.listen(3031,()=>{
+app.listen(3030,()=>{
     console.log("server started")
 })
